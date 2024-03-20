1b038a998b2562b056886aef18d139abc3c3aadb
joshu <joshuali20051210@gmail.com>
2024-03-20 11:55:27 -0700

    last one

--- core/src/com/yueshuya/MyGdxGame.java
+++ core/src/com/yueshuya/MyGdxGame.java
@@ -3,6 +3,11 @@ package com.yueshuya;
 import com.badlogic.gdx.Game;
 
 public class MyGdxGame extends Game {
+	/*TODO
+	*  radom annla genration
+	*  beartification
+	*  UI
+	*  Famer inter face */
 
 	@Override
 	public void create() {
